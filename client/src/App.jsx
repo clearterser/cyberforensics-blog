b9a2a3f1918a46b13536c2d01fabcb0149242c8b
clearterser <clear.terser.35@icloud.com>
2025-02-24 23:18:44 +0800

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-800">
+        Hello, Cyberforensics Blog
+      </h1>
+    </div>
+  )
+}
